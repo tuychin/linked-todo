863484486c17568decccac6f8022e9821866a837
Ravil <tuychin@gmail.com>
2022-03-30 23:51:40 +0300

    feat docs api (get)

--- pages/api/v1/docs/index.ts
+++ pages/api/v1/docs/index.ts
@@ -3,13 +3,13 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const tasks = await db.collection('tasks').orderBy('created').get();
-    const tasksData = tasks.docs.map(task => ({
-      id: task.id,
-      ...task.data()
+    const docs = await db.collection('docs').orderBy('created').get();
+    const docsData = docs.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
     }));
 
-    res.status(200).json({tasksData});
+    res.status(200).json({docsData});
   } catch (evt) {
     res.status(400).end();
   }
