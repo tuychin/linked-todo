5ccb93912c806b03d02a4a190c0a6d29c9a2fa37
Ravil <tuychin@gmail.com>
2022-04-01 18:56:12 +0300

    change api path

--- pages/api/v1/topics/index.ts
+++ pages/api/v1/topics/index.ts
@@ -3,13 +3,13 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const docs = await db.collection('docs').orderBy('created').get();
-    const docsData = docs.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
+    const topics = await db.collection('topics').orderBy('created').get();
+    const topicsData = topics.topics.map(topic => ({
+      id: topic.id,
+      ...topic.data()
     }));
 
-    res.status(200).json({docsData});
+    res.status(200).json({topicsData});
   } catch (evt) {
     res.status(400).end();
   }
